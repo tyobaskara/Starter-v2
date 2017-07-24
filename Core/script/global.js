02ea358d779ab1ab0d438d498cb83abce7a104d0
tyobaskara <prasetyaajibaskara@gmail.com>
2017-07-24 20:37:10 +0700

    updates

--- Core/script/global.js
+++ Core/script/global.js
@@ -1,19 +1,13 @@
+(function($) {
 
-
-
-
-(function($){
-
- 	if($('[placeholder]').length > 0){
+    if ($('[placeholder]').length > 0) {
         $('input, textarea').placeholder();
     }
-
-    $("h1").css("color", "red");
 
     console.log("Slick = " + typeof(slick));
 
     //if(typeof(jcf)!=="undefined") {
-	    jcf.replaceAll();
-	//}
+    jcf.replaceAll();
+    //}
 
 })(jQuery);
