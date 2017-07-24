9bdc9f8cfc0600579d81b4154deb308556843d02
tyobaskara <prasetyaajibaskara@gmail.com>
2017-07-24 20:26:15 +0700

    update gulpfile

--- Core/gulpfile.js
+++ Core/gulpfile.js
@@ -14,49 +14,48 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     runs = require('run-sequence'),
     clean = require('gulp-clean'),
-    injectPartials = require('gulp-file-include'),
-    uncss = require('gulp-uncss');
+    injectPartials = require('gulp-file-include');
+//uncss = require('gulp-uncss');
 
 var targetPath = "../Public";
 
 var paths = {
-    sass:
-        [	
-        	//VENDOR
-            path.join(__dirname, "sass/*bootstrap*/_bootstrap.scss"),
+    sass: [
+        //VENDOR
+        path.join(__dirname, "sass/*bootstrap*/_bootstrap.scss"),
 
-            //PLUGINS
-            path.join(__dirname, "bower_components/jcf/dist/css/theme-minimal/jcf.css"),
-            //path.join(__dirname, "bower_components/jcf/dist/css/demo.css"),
-            path.join(__dirname, "bower_components/slick-carousel/slick/slick.css"),
-            path.join(__dirname, "bower_components/slick-carousel/slick/slick-theme.css"),
-            // FONT AWESOME CSS
-            path.join(__dirname, "bower_components/font-awesome/css/font-awesome.css"),
-            path.join(__dirname, "plugins/**/*.css"),
+        //PLUGINS
+        path.join(__dirname, "bower_components/jcf/dist/css/theme-minimal/jcf.css"),
+        //path.join(__dirname, "bower_components/jcf/dist/css/demo.css"),
+        path.join(__dirname, "bower_components/slick-carousel/slick/slick.css"),
+        path.join(__dirname, "bower_components/slick-carousel/slick/slick-theme.css"),
+        // FONT AWESOME CSS
+        path.join(__dirname, "bower_components/font-awesome/css/font-awesome.css"),
+        path.join(__dirname, "plugins/**/*.css"),
 
-            //CUSTOM
-            path.join(__dirname, 'sass/*.scss'),
-            path.join(__dirname, 'sass/*pages*/*.scss')
-        ],      
+        //CUSTOM
+        path.join(__dirname, 'sass/*.scss'),
+        path.join(__dirname, 'sass/*pages*/*.scss')
+    ],
     js: [
-    		//VENDOR
-            //path.join(__dirname, "bower_components/javascript/defer_parsing.js"),
-            path.join(__dirname, "bower_components/jquery/dist/jquery.min.js"),
-            path.join(__dirname, "bower_components/bootstrap/dist/js/bootstrap.min.js"),
+        //VENDOR
+        //path.join(__dirname, "bower_components/javascript/defer_parsing.js"),
+        path.join(__dirname, "bower_components/jquery/dist/jquery.min.js"),
+        path.join(__dirname, "bower_components/bootstrap/dist/js/bootstrap.min.js"),
 
-            //PLUGINS
-            path.join(__dirname, "bower_components/jcf/dist/js/jcf.js"),
-            path.join(__dirname, "bower_components/jcf/dist/js/jcf.radio.js"),
-            path.join(__dirname, "bower_components/jcf/dist/js/jcf.checkbox.js"),
-            path.join(__dirname, "bower_components/jcf/dist/js/jcf.select.js"),
-            path.join(__dirname, "bower_components/slick-carousel/slick/slick.min.js"),
+        //PLUGINS
+        path.join(__dirname, "bower_components/jcf/dist/js/jcf.js"),
+        path.join(__dirname, "bower_components/jcf/dist/js/jcf.radio.js"),
+        path.join(__dirname, "bower_components/jcf/dist/js/jcf.checkbox.js"),
+        path.join(__dirname, "bower_components/jcf/dist/js/jcf.select.js"),
+        path.join(__dirname, "bower_components/slick-carousel/slick/slick.min.js"),
 
-            path.join(__dirname, "plugins/**/*.js"),
+        path.join(__dirname, "plugins/**/*.js"),
 
-            //APP
-            path.join(__dirname, 'script/*.js'),
-            path.join(__dirname, 'script/*Includes*/*.js')
-    ]	
+        //APP
+        path.join(__dirname, 'script/*.js'),
+        path.join(__dirname, 'script/*Includes*/*.js')
+    ]
 }
 
 // --------------------------------------------------------- INIT TASK //
@@ -67,20 +66,20 @@ gulp.task('init', ['sass', 'js']);
 
 
 // CLEANER
-gulp.task('clean-sass', function (file) {
-  return gulp.src([
-        path.join(__dirname, targetPath + '/assets/css/style.min.css'), 
-        path.join(__dirname, targetPath + '/assets/css/maps/style.min.css.map'), 
-        ], {read: false})
-    .pipe(clean({force: true}));
+gulp.task('clean-sass', function(file) {
+    return gulp.src([
+            path.join(__dirname, targetPath + '/assets/css/style.min.css'),
+            path.join(__dirname, targetPath + '/assets/css/maps/style.min.css.map'),
+        ], { read: false })
+        .pipe(clean({ force: true }));
 });
-gulp.task('clean-js', function () {
-  return gulp.src(path.join(__dirname, targetPath + '/assets/js/app.min.js'), {read: false})
-    .pipe(clean({force: true}));
+gulp.task('clean-js', function() {
+    return gulp.src(path.join(__dirname, targetPath + '/assets/js/app.min.js'), { read: false })
+        .pipe(clean({ force: true }));
 });
-gulp.task('clean-csspages', function () {
-  return gulp.src(path.join(__dirname, targetPath + '/assets/css/pages/*.css'), {read: false})
-    .pipe(clean({force: true}));
+gulp.task('clean-csspages', function() {
+    return gulp.src(path.join(__dirname, targetPath + '/assets/css/pages/*.css'), { read: false })
+        .pipe(clean({ force: true }));
 });
 // CLEANER
 
@@ -89,8 +88,8 @@ gulp.task('html', function() {
     return gulp.src(path.join(__dirname, "views/pages/*.html"))
         //.pipe(html())
         .pipe(injectPartials({
-          prefix: '@@',
-          basepath: '@file'
+            prefix: '@@',
+            basepath: '@file'
         }))
         .pipe(gulp.dest(path.join(__dirname, targetPath)))
         .pipe(browserSync.stream())
@@ -99,7 +98,7 @@ gulp.task('html', function() {
 // copy the sass file task
 gulp.task('sass', ['clean-sass'], function() {
     return gulp.src([
-    		//VENDOR
+            //VENDOR
             //'bower_components/bootstrap/dist/css/bootstrap.min.css',
             'sass/*bootstrap*/_bootstrap.scss',
 
@@ -114,14 +113,14 @@ gulp.task('sass', ['clean-sass'], function() {
 
             //CUSTOM
             'sass/*.scss'
-    	])
+        ])
         .pipe(concat('style.min.css'))
         .pipe(sourcemaps.init())
         .pipe(sass({
-          outputStyle: 'compressed',
-          includePaths: bourbon.includePaths
+            outputStyle: 'compressed',
+            includePaths: bourbon.includePaths
         }).on('error', sass.logError))
-        .pipe(postcss([autoprefixer({ browsers: ['> 0%'] })]))       
+        .pipe(postcss([autoprefixer({ browsers: ['> 0%'] })]))
         //.pipe(uglifycss())
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(path.join(__dirname, targetPath + '/assets/css/')))
@@ -129,9 +128,9 @@ gulp.task('sass', ['clean-sass'], function() {
 });
 
 // copy javascript task
-gulp.task('js', ['clean-js'], function () {
+gulp.task('js', ['clean-js'], function() {
     return gulp.src([
-    		//VENDOR
+            //VENDOR
             //'bower_components/javascript/defer_parsing.js',
             'bower_components/jquery/dist/jquery.min.js',
             'bower_components/bootstrap/dist/js/bootstrap.min.js',
@@ -147,7 +146,7 @@ gulp.task('js', ['clean-js'], function () {
             //APP
             'script/*.js',
             'script/*Includes*/*.js'
-    	])
+        ])
         .pipe(concat('app.min.js'))
         .pipe(uglifyjs())
         .pipe(gulp.dest(path.join(__dirname, targetPath + '/assets/js/')))
@@ -157,15 +156,15 @@ gulp.task('js', ['clean-js'], function () {
 // --------------------------------------------------------- SET SHOW WATCHER //
 
 //Watcher active
-gulp.task('watch', function(){
+gulp.task('watch', function() {
     watch(path.join(__dirname, "sass/**/*.scss"), function() { runs('sass'); });
     watch(paths.sass, function() { runs('sass'); });
-    watch(paths.js, function() { runs('js'); });  
+    watch(paths.js, function() { runs('js'); });
 });
 
 // --------------------------------------------------------- SHOW WATCHER //
 
-gulp.task('default', ['init', 'html', 'watch'], function(){
+gulp.task('default', ['init', 'html', 'watch'], function() {
 
     browserSync.init({
         server: {
@@ -184,7 +183,7 @@ gulp.task('development', ['init', 'watch']);
 
 
 //UNCSS
-gulp.task('uncss', function () {
+gulp.task('uncss', function() {
     return gulp.src([
             //CUSTOM
             'sass/*.scss'
@@ -192,8 +191,8 @@ gulp.task('uncss', function () {
         .pipe(concat('uncss.css'))
         .pipe(sourcemaps.init())
         .pipe(sass({
-          outputStyle: 'compressed',
-          includePaths: bourbon.includePaths
+            outputStyle: 'compressed',
+            includePaths: bourbon.includePaths
         }).on('error', sass.logError))
         .pipe(uncss({
             html: [path.join(__dirname, targetPath + '/*.html')],
