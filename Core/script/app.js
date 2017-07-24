9eb842461ba55a07fa389a57ae7079871dfacfd8
tyobaskara <prasetyaajibaskara@gmail.com>
2017-07-24 22:38:12 +0700

    update js

--- Core/script/app.js
+++ Core/script/app.js
@@ -1,4 +1,14 @@
 (function($) {
 
+    alert
+
+    if($('.select-v1').length > 0) {
+        var select = $('.select-v1');
+
+        select.change(function(){
+            var _this = $(this).val();
+            alert('select is changed with val of ' + _this );
+        });
+    }
 
 })(jQuery);
