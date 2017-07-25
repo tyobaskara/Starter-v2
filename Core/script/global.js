93287e553fd852576e9c616393a3db1a0824e0c7
tyobaskara <prasetyaajibaskara@gmail.com>
2017-07-25 12:03:20 +0700

    update plugins

--- Core/script/global.js
+++ Core/script/global.js
@@ -1,7 +1,22 @@
 (function($) {
+    
+    var myLazyLoad = new LazyLoad();
+
+    new WOW().init();
 
     if ($('[placeholder]').length > 0) {
         $('input, textarea').placeholder();
+    }
+
+    if($('.tslider').length > 0 ) {
+        var slider1 = $('.tslider');
+
+        slider1.slick({
+            lazyLoad: 'ondemand',
+            slidesToShow: 2,
+            slidesToScroll: 1
+        });
+	
     }
 
     console.log("Slick = " + typeof(slick));
